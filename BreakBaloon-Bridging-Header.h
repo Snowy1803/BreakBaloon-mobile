783333d054ad8bbb6b935e84040e8a14360a0aad
Emil <emil@macbook-air-de-charlotte.local>
2016-07-05 22:41:11 +0200

    Just added the bridge to github

---
+++ BreakBaloon-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  BreakBaloon-Bridging-Header.h
+//  BreakBaloon
+//
+//  Created by Emil on 05/07/2016.
+//  Copyright © 2016 Snowy_1803. All rights reserved.
+//
+
+#ifndef BreakBaloon_Bridging_Header_h
+#define BreakBaloon_Bridging_Header_h
+
+#import "SSZipArchive.h"
+
+#endif /* BreakBaloon_Bridging_Header_h */
