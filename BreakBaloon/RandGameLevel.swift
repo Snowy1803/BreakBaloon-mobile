99b867bc286bc0bfe7480506672fde776683e35c
Emil <emil@macbook-air-de-charlotte.local>
2016-07-31 17:34:20 +0200

    RB: Added 3 levels

--- BreakBaloon/RandGameLevel.swift
+++ BreakBaloon/RandGameLevel.swift
@@ -10,7 +10,7 @@ import Foundation
 import SpriteKit
 
 class RandGameLevel: SKSpriteNode {
-    static let levels:[(UInt, NSTimeInterval, NSTimeInterval, UInt, UInt)] = [(10, 1.25, 4, 1, 2), (10, 0.75, 4, 1, 2), (30, 0.75, 3.5, 5, 3), (25, 0.75, 3, 3, 3), (35, 0.75, 2.5, 1, 3), (35, 0.75, 2.25, 0, 3), (40, 0.5, 2, 2, 4)]
+    static let levels:[(UInt, NSTimeInterval, NSTimeInterval, UInt, UInt)] = [(10, 1.25, 4, 1, 2), (10, 0.75, 4, 1, 2), (30, 0.75, 3.5, 5, 3), (25, 0.75, 3, 3, 3), (35, 0.75, 2.5, 1, 3), (35, 0.75, 2.25, 0, 3), (40, 0.5, 2, 2, 4), (50, 0.5, 2, 1, 5), (50, 0.5, 1.5, 0, 5), (75, 0.5, 1.5, 0, 4)]
     
     var next:RandGameLevel?
     let index:Int
